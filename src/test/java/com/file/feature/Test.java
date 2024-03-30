247a805fd7e4e3cfbe748d6b76be8052376d5e31
unknown <gamestergp000@gmail.com>
2024-03-30 09:32:34 +0530

    new changes

--- src/test/java/com/file/feature/Test.java
+++ src/test/java/com/file/feature/Test.java
@@ -2,12 +2,12 @@ package com.file.feature;
 
 import com.intuit.karate.junit5.Karate;
 
-public class runner {
+public class Test {
 	
 	@Karate.Test
     Karate runAll1() {
 		//System.setProperty("karate.env", "dev");
-       return Karate.run("demo1").relativeTo(getClass());
+       return Karate.run().relativeTo(getClass());
     }
 	
 }
